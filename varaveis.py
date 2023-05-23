ed1cc98bffb81119179d4d647df7f157617bbe45
Murilo <murilormleal@outlook.com>
2023-05-23 20:25:03 -0300

    variaveis em python

---
+++ varaveis.py
@@ -0,0 +1,30 @@
+# Variaveis e operadores
+
+# 1. Variaveis
+
+idade = 20
+nome = "Murilo"
+altura = 1.77
+estudando = True
+
+print(nome, "tem ", idade," anos de idade")
+
+print(type(idade))
+print(type(altura))
+print(type(nome))
+print(type(estudando))
+
+"""
+Tipos de variáveis
+1. int: números inteiros, ou seja, números sem parte decimal: e, 5, -1;
+2. float: números reais, ou seja, números com parte decimal: 1.0, -2.7;
+3. str: cadeias de caracteres, ou seja, dados textuais (string);
+4. bool: val res lógicos (booleanos): True ou False;
+
+"""
+
+# Obtendo valores e armazenando
+
+
+ling = input("Qual a linguagem que vc esta estudando? ")
+print("Voce estuda a linguagem", ling)
