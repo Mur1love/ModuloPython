fb1503c3f0a45c2d0fefcb6f1b5962d881c8e124
Murilo <murilormleal@outlook.com>
2023-05-24 09:20:13 -0300

    condicionais

---
+++ condicionais.py
@@ -0,0 +1,22 @@
+#Condicionais 
+
+"""idade = int(input("Digite a sua idade: "))
+
+if idade >= 18:
+    print("Voce e maior de idade!")
+else:
+    print("Voce e menor de idade!")"""
+
+
+med = 6
+pres = 88
+
+if med >= 7 and pres >=70:
+    print("Voce foi aprovado!")
+elif pres < 70:
+    print("Voce foi reprovado por falta!")
+else:
+    print("Voce foi reprovado!")
+
+
+
