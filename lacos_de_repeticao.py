65c19d9463afb87d90d3e4feabf74fd4122cc611
Murilo <murilormleal@outlook.com>
2023-05-24 10:18:20 -0300

    repeticao

---
+++ lacos_de_repeticao.py
@@ -0,0 +1,41 @@
+#Lacos de Repeticao
+
+numeroSorteado = 15
+
+"""numeroEscolhido = int(input("Digite um numero entre 1 e 20: "))
+
+while numeroEscolhido != numeroSorteado:
+    print("Errou, tente novamente...")
+    numeroEscolhido = int(input("Digite um numero entre 1 e 20: "))
+
+print("Parabens, vc acertou!")"""
+
+
+#Estrutura com o contador
+
+"""i=0
+while i <= 5:
+    print(i)
+    i += 1
+
+print("Fim")"""
+
+#EStruturas com FOR
+
+
+"""for i in range(10): 
+    print(i)"""
+
+"""for i in range(1, 10): 
+    print(i)"""
+
+"""for i in range(0, 12, 3): 
+    print(i)"""
+
+soma = 0
+for i in range(1, 4):
+    n = float(input(f"Digite sua nota {i}: "))
+    soma += n
+
+print(soma / 3)
+
