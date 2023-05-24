0801d7905a54215a488d82e09270d616336193e8
Murilo <murilormleal@outlook.com>
2023-05-24 08:57:20 -0300

    conversao de tipos

---
+++ conversao_de_tipos.py
@@ -0,0 +1,21 @@
+#Convertendo Tipos
+
+idade = "10"
+n1 = "20"
+n2 = "50"
+
+#print(n1 + n2) #Concatena ao inves de somar, pois sao Strings
+
+idadeInt = int(idade) #Converte de String para Inteiro
+
+#Funcoes de Convercao
+
+#int()
+#bool()
+#str()
+#float()
+
+
+# Todo INPUT em Python recebe String
+
+altura = float(input = ("Digite sua altura: ")) #Muda o input para receber FLOAT
