9dca2d709ebb2a57871d3f3b2e29aa34d06b5547
Murilo <murilormleal@outlook.com>
2023-05-24 08:42:53 -0300

    operadores

---
+++ operadores.py
@@ -0,0 +1,40 @@
+# Operações Matemáticas
+
+"""
+
+— Soma : +
+- Subtração: -
+- Multiplicação: *
+- Divisão: /
+- Divisão inteira: //
+- Resto da divisão: %
+- Potência: **
+
+"""
+
+n1 = 10
+n2 = 20
+
+print(n1 + n2)
+print(n1 - n2)
+print(n1 * n2)
+print(n1 / n2)
+print(n1 // n2)
+print(20 % 3)
+print(2 ** 3)
+
+#Operadores Booleanos
+
+idade1 = 15
+idade2 = 20
+altura1 = 1.77
+altura2 = 1.65
+altura3 = altura2
+
+print(idade1 > idade2) #false
+print(idade1 <= idade2) #true
+print("Python" == "python") #false
+print("Banana" != "Abacaxi") #true
+print(altura1 >= altura2) #true
+print(altura2 > altura3) #false
+
